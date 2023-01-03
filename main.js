1f420ebf40ec456116eae7f101db0ba5cdf9d16e
tainasouz <souzataina5657@gmail.com>
2023-01-03 10:58:40 -0300

    Altera o fetch e o link da Api para tendências

--- main.js
+++ main.js
@@ -1,42 +1,29 @@
-let cardsFilme = document.querySelector(".card-filme")
+let allData = []
 
-let urlFilme = "https://api.themoviedb.org/3/movie/popular?api_key=f1b9e75f6dc0efa5b1181f25004a24b4&language=pt-BR"
-
-fetch(urlFilme)
-    .then((response) => response.json())
-    .then((filmes) => {
-        carregarfilmes(filmes)
-    })
+let urlData = "https://api.themoviedb.org/3/trending/all/week?api_key=f1b9e75f6dc0efa5b1181f25004a24b4&page="
 
 
-carregarfilmes = (data) => {
-    let HTML = ``
-    let filme
+async function fetchData() {
+    for (let i = 1; i <= 10; i++) {
+        const response = await fetch(urlData + i);
+        const data = await response.json();
 
-    for (let i = 0; i < 12; i++) {
+        allData = allData.concat(data.results)
 
-        filme = data.results[i]
-
-
-
-
-        HTML += `
-            <div class="card">
-                <div class="img-card">
-                    <img src="https://image.tmdb.org/t/p/w200/${filme.poster_path}" alt="Filme ${filme.title}">
-                </div>
-                <div class="info-card">
-                    <h3 class="titulo-filme">${filme.title}</h3>
-                    <h4 class="genero-filme">Gênero Filme</h4>
-                    <div class="avaliacao-filme">
-                        <i class="fi fi-rr-star estrela"></i>
-                        <h4 class=num-card>${filme.vote_average}</h4>
-                    </div>
-                </div>
-            </div>
-        `
     }
-
-    document.querySelector(".cards").innerHTML = HTML
+    carregarDados(allData)
 }
 
+fetchData().then(data => {
+    data;
+});
+
+carregarDados = (dados) => {
+    let htmlFilmes = ``
+    let htmlSerie = ``
+
+    for (let i = 0; i < 24; i++) {
+        
+        
+    }
+}
