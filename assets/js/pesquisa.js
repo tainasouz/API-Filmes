805f28f837de0c9967a5532443a2214b4d18415c
tainasouz <souzataina5657@gmail.com>
2023-01-24 10:29:38 -0300

    Nova funcionalidade: Carrega e exibe resultados na página de pesquisa

--- assets/js/pesquisa.js
+++ assets/js/pesquisa.js
@@ -1,107 +1,126 @@
 const urlParams = new URLSearchParams(window.location.search);
 const search = urlParams.get("search")
 
-if (search == "") {
-  document.querySelector(".titulo").innerHTML = `Não foi encontrado nenhum resultado para "${search}"`
-}
-
 const urlSearch = `https://api.themoviedb.org/3/search/multi?api_key=6c0b4180230783f9b7199576cb4504dc&language=pt-BR&query=${search}&include_adult=false`
 
-function pagination_fetch(
+
+
+window.onload = () => {
+  carregaTodosOsDados().then((listaResultados) => paginacao(listaResultados))
+}
+
+
+function carregaTodosOsDados(
   url = urlSearch,
   page = 1,
   Response = []
 ) {
-  fetch(`${url}&page=${page}`)
+  return fetch(`${url}&page=${page}`)
     .then(response => response.json())
     .then(data => data.results)
     .then(results => {
-      const response = [...Response, ...results];
+      const response = [...Response, ...results.filter(e => e.media_type != "person")];
 
-      if (results.length !== 0) {
+      if (results.length !== 0 && response.length < 100) {
         page++;
 
-        return pagination_fetch(url, page, response);
+        return carregaTodosOsDados(url, page, response);
       }
 
       return response;
     })
-    .then(res => {
-      console.log(res)
+}
 
-      if (res.length > 0) {
+var pagina = 0
+var numPaginas
 
-        document.querySelector(".titulo").innerHTML = `Resultados para "${search}"`
+function paginacao(dados) {
 
-        html = ``
+  console.log(dados)
 
-        const dados = res.filter(e => e.media_type != "person")
-
-        for (let i = 0; i < 10; i++) {
-
-          let resultado = dados[i]
-          let scr
-          let date
+  numPaginas = Math.ceil(dados.length / 10)
+  const resultadoPagina = dados.slice(pagina * 10, (pagina + 1) * 10)
 
 
-          if (resultado.name == undefined) {
-            resultado.name = resultado.title
-          }
-
-          if (resultado.poster_path == undefined) {
-            scr = "/assets/img/poster.png"
-          }
-          else {
-            scr = `https://image.tmdb.org/t/p/original/${resultado.poster_path}`
-          }
-
-          if (resultado.media_type == "movie") {
-            date = resultado.release_date
-          }
-          else {
-            date = resultado.first_air_date
-          }
-
-          let dataFormat = date.replace(/(\d*)-(\d*)-(\d*).*/, '$3/$2/$1')
-
-          html += `
-            <div class="card-result">
-                <div class="poster">
-                    <img src="${scr}" alt="" >
-                </div>
-                <div class="info">
-                <div class="header-card">
-                    <div class="titulo-card">
-                      ${resultado.name}
-                    </div>
-                    <div class="data-lancamento">
-                      ${dataFormat}
-                    </div>
-                </div>
-                <div class="body-card">
-                    <div class="sinopse">
-                      ${resultado.overview}
-                    </div>
-                </div>
-                </div>
-            </div>`
-            
-            document.querySelector(".resultados").innerHTML = html
-
-        }
-        
-      }
-      else{
-        document.querySelector(".titulo").innerHTML = `Não foi encontrado nenhum resultado para "${search}"`
-      }
-
-
-
-    })
+  if (dados.length > 0) {
+    document.querySelector(".titulo").innerHTML = `Resultados para "${search}"`
+    carregaResultados(resultadoPagina, pagina)
+  }
 
 }
 
-pagination_fetch()
 
+
+function carregaResultados(dados) {
+
+  let html = ``
+
+
+  for (let i = 0; i < dados.length; i++) {
+
+    let resultado = dados[i]
+    let scr
+    let date
+
+
+
+    if (resultado.name == undefined) {
+      resultado.name = resultado.title
+    }
+
+    if (resultado.poster_path == undefined) {
+      scr = "/assets/img/poster.png"
+    }
+    else {
+      scr = `https://image.tmdb.org/t/p/original/${resultado.poster_path}`
+    }
+
+    if (resultado.media_type == "movie") {
+      date = resultado.release_date
+    }
+    else {
+      date = resultado.first_air_date
+    }
+
+    let dataFormat = date.replace(/(\d*)-(\d*)-(\d*).*/, '$3/$2/$1')
+
+    html += `
+        <div class="card-result">
+            <div class="poster">
+                <img src="${scr}" alt="" >
+            </div>
+            <div class="info">
+            <div class="header-card">
+                <div class="titulo-card">
+                  ${resultado.name}
+                </div>
+                <div class="data-lancamento">
+                  ${dataFormat}
+                </div>
+            </div>
+            <div class="body-card">
+                <div class="sinopse">
+                  ${limitaTamSinopse(resultado.overview)}
+                </div>
+            </div>
+            </div>
+        </div>`
+
+    document.querySelector(".resultados").innerHTML = html
+
+  }
+
+}
+
+
+
+function limitaTamSinopse(texto) {
+
+  if (texto.length > 380) {
+    return `${texto.slice(0, 380)}...`
+  }
+  return texto
+
+}
 
 
