25444a618904c999f3e2f363acb288c7292eb591
tainasouz <souzataina5657@gmail.com>
2023-03-27 13:57:12 -0300

    feat: componete body página de pesquisa

--- frontend/front-react-dom/src/components/BodyPesquisa/index.tsx
+++ frontend/front-react-dom/src/components/BodyPesquisa/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { ResultadoPesquisa } from '../../type';
+import Card from '../Card';
 import './style.css'
 
 interface PesquisaType {
@@ -19,13 +20,42 @@ function BodyPesquisa({ query }: PesquisaType) {
             })
     }, [query])
 
+    state.sort(function(a, b) {
+         
+        if (a.vote_count < b.vote_count) {
+          return 1;
+        }
+        if (a.vote_count > b.vote_count) {
+          return -1;
+        }
+        if (a.vote_average < b.vote_average) {
+          return 1;
+        }
+        if (a.vote_average > b.vote_average) {
+          return -1;
+        }
+        
+        return 0;
+      });
+
     return (
-        <>
+        <div className="body">
             <div className="titulo ">Resultados para {query}</div>
             <div className="resultados">
-                {JSON.stringify(state)}
+                {
+                    state.length === 0
+                    ? <></>
+                    // ?  Array.from({length: 10}).map(() => <SkeletonTeste />)
+
+                    : state.map((resultado) => {
+                        return (
+                            <Card key={`${resultado.id}-${resultado.media_type}-${resultado.name || resultado.title}`} id={resultado.id} poster_path={resultado.poster_path} title={resultado.title || resultado.name} vote_average={resultado.vote_average} type={resultado.media_type}/>
+                        )
+                    })
+                }
+            
             </div>
-        </>
+        </div>
     );
 }
 
--- frontend/front-react-dom/src/routes/pesquisa.jsx
+++ frontend/front-react-dom/src/routes/pesquisa.jsx
@@ -6,11 +6,13 @@ import BodyPesquisa from '../components/BodyPesquisa';
 function Pesquisa() {
 
     const query = new URLSearchParams(location.search).get('search');
-    console.log(query);
 
     return (
         <>
-            <Navbar backgrounColor="#ff0000"/>
+        <div className="header" style={{backgroundColor: 'black'}}>
+            <Navbar color="#0e0e0e"/>
+        </div>
+            
             <BodyPesquisa query={query} />
         </>
     );
