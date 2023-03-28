bc669ea83ab2ca53af1d7bfb3ec6130123a4e288
tainasouz <souzataina5657@gmail.com>
2023-03-28 10:33:33 -0300

    feat: Adiciona Loader e Ordena Array de resultados

--- frontend/front-react-dom/src/components/BodyPesquisa/index.tsx
+++ frontend/front-react-dom/src/components/BodyPesquisa/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ResultadoPesquisa } from '../../type';
 import Card from '../Card';
+import Loader from '../Loader';
 import './style.css'
 
 interface PesquisaType {
@@ -11,7 +12,7 @@ interface PesquisaType {
 
 function BodyPesquisa({ query }: PesquisaType) {
 
-    const [state, setState] = useState<Array<ResultadoPesquisa>>([])
+    const [state, setState] = useState<Array<ResultadoPesquisa> | null>(null)
     useEffect(() => {
         fetch(`http://localhost:3000/pesquisa/${query}/1`)
             .then(res => res.json())
@@ -20,41 +21,53 @@ function BodyPesquisa({ query }: PesquisaType) {
             })
     }, [query])
 
-    state.sort(function(a, b) {
-         
+    if (state !== null) {
+        state.sort(function (a, b) {
+
         if (a.vote_count < b.vote_count) {
-          return 1;
+            return 1;
         }
         if (a.vote_count > b.vote_count) {
-          return -1;
+            return -1;
         }
         if (a.vote_average < b.vote_average) {
-          return 1;
+            return 1;
         }
         if (a.vote_average > b.vote_average) {
-          return -1;
+            return -1;
         }
-        
+
         return 0;
-      });
+    });
+    }
+
 
     return (
         <div className="body">
-            <div className="titulo ">Resultados para {query}</div>
-            <div className="resultados">
-                {
+            {
+                state === null
+                    ?
+                    <Loader/>
+                    :
                     state.length === 0
-                    ? <></>
-                    // ?  Array.from({length: 10}).map(() => <SkeletonTeste />)
+                        ?
+                        <div className="titulo ">Não foi encontrado nenhum resultado para '{query}'</div>
+                        :
+                        <>
+                            <div className="titulo ">Resultados para '{query}'</div>
+                            <div className="resultados">
+                                {
+                                    state.map((resultado) => {
+                                        return (
+                                            <Card key={`${resultado.id}-${resultado.media_type}-${resultado.name || resultado.title}`} id={resultado.id} poster_path={resultado.poster_path} title={resultado.title || resultado.name} vote_average={resultado.vote_average} type={resultado.media_type} />
+                                        )
+                                    })
+                                }
+                            </div>
+                        </>
 
-                    : state.map((resultado) => {
-                        return (
-                            <Card key={`${resultado.id}-${resultado.media_type}-${resultado.name || resultado.title}`} id={resultado.id} poster_path={resultado.poster_path} title={resultado.title || resultado.name} vote_average={resultado.vote_average} type={resultado.media_type}/>
-                        )
-                    })
-                }
-            
-            </div>
+            }
+
         </div>
     );
 }
